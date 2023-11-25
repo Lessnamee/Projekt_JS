ff864dcc36bbfa32df3beae03bea6319a0c9dc98
cinnamoon <patoswiecimska@gmail.com>
2023-11-25 23:31:21 +0100

    commit

---
+++ calendar.js
@@ -0,0 +1,21 @@
+var lekarzCell = document.getElementById("lekarzCell");
+
+  //nasłuchiwanie kliknięcia dla lekarza Jan Kowalski
+  document.getElementById("lekarz1").addEventListener("click", function() {
+    window.location.href = "lekarz1.html";
+  });
+
+  //nasłuchiwanie kliknięcia dla lekarza Anna Nowak
+  document.getElementById("lekarz2").addEventListener("click", function() {
+    window.location.href = "lekarz2.html";
+  });
+  //nasłuchiwanie kliknięcia dla lekarza Maria Wiśniewska
+  document.getElementById("lekarz3").addEventListener("click", function() {
+    window.location.href = "lekarz3.html";
+  });
+
+  // Dodaj nasłuchiwanie kliknięcia dla lekarza Piotr Nowak
+  document.getElementById("lekarz4").addEventListener("click", function() {
+    window.location.href = "lekarz4.html";
+  });
+
